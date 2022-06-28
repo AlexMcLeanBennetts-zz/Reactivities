853f525fcf8fc6945ab81125a41adf4e94ec5a71
Alex McLean <alex.mclean@bennetts.co.uk>
2022-06-28 15:03:27 +0100

    Adds CRUD functionality

---
+++ Application/Activities/List.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> {}
+        public class Hander : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+
+            public Hander(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _context.Activities.ToListAsync();
+            }
+        }
+
+    }
+}
