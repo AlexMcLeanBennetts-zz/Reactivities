cdf757bd3772a998b5afd0727844507392a44df4
Alex McLean <alex.mclean@bennetts.co.uk>
2022-07-12 15:02:09 +0100

    change to src for logo

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
         <header className="flex justify-between sticky pl-10 pr-24 py-4 bg-gradient-to-br from-[#182a73] to-[#20a7ac]">
             <div className="w-10">
                 <NavLink to='/'>
-                    <img src="images/logo.png" alt="Reactivities" />
+                    <img src="/images/logo.png" alt="Reactivities" />
                 </NavLink>
             </div>
             <ul className="flex text-amber-50 items-center">
