a105956939b96720a1e569101578e3a60a6bb812
Alex McLean <alex.mclean@bennetts.co.uk>
2022-07-13 10:11:45 +0100

    adds validation to startup

--- API/Startup.cs
+++ API/Startup.cs
@@ -17,6 +17,7 @@ using MediatR;
 using Application.Activities;
 using Application.Core;
 using API.Extensions;
+using FluentValidation.AspNetCore;
 
 namespace API
 {
@@ -34,7 +35,10 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation(config =>
+            {
+                config.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
             services.AddApplicationServices(_config);
         }
 
