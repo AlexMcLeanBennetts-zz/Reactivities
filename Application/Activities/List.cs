17dad308c76e6b2ddb3616a28b8b487225cd873b
Alex McLean <alex.mclean@bennetts.co.uk>
2022-08-02 11:22:18 +0100

    Allows attendees to be stores within activity

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -13,19 +15,26 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<Activity>>> {}
-        public class Hander : IRequestHandler<Query, Result<List<Activity>>>
+        public class Query : IRequest<Result<List<ActivityDto>>> { }
+        public class Hander : IRequestHandler<Query, Result<List<ActivityDto>>>
         {
             private readonly DataContext _context;
+            private readonly IMapper _mapper;
 
-            public Hander(DataContext context)
+            public Hander(DataContext context, IMapper mapper)
             {
+                _mapper = mapper;
                 _context = context;
             }
 
-            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync());
+
+                var activities = await _context.Activities
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync(cancellationToken);
+
+                return Result<List<ActivityDto>>.Success(activities);
             }
         }
 
